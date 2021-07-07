5068ed1f916187e334b667564799ec05b0711fae
Bruncafc1992 <joseabjerez@hotmail.com>
2021-07-07 18:57:34 -0500

    Ejercicio cineteca

---
+++ 3_herencia/Cineteca/Documental.js
@@ -0,0 +1,11 @@
+// IMPORTO UNA CLASE DE UN ARCHIVO
+
+const Largometraje = require('./Largometraje');
+
+class Documental extends Largometraje{
+    constructor(titulo, duracion) {
+        super(titulo, duracion);
+        
+    }
+
+}
---
+++ 3_herencia/Cineteca/Largometraje.js
@@ -0,0 +1,13 @@
+
+class Largometraje {
+    constructor(titulo, duracion) { 
+        this.titulo = titulo;
+        this.duracion = duracion;
+    };
+
+
+}
+
+//IMPORTO O PONGO DISPONIBLE UN ARCHIVO PARA QUE OTRO PUEDA ACCEDER
+
+module.exports = Largometraje;
