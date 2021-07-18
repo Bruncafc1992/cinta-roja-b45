59d9dd765eba42218460e2be501c401259907c26
Bruncafc1992 <joseabjerez@hotmail.com>
2021-07-17 23:50:01 -0500

    callbacks

--- 4_async/callback.js
+++ 4_async/callback.js
@@ -1,2 +1,47 @@
-// ES UNA FUNCION QUE SE PASA COMO ARGUMENTO DE OTRA FUNCION
+// CALLBACK --> ES UNA FUNCION QUE SE PASA COMO ARGUMENTO DE OTRA FUNCIÓN
 
+//FUNCION INICIAL
+let mostrarUnMensaje = (funcion)=>{
+    funcion()
+};
+
+//FUNCION CALLBACK 
+let yoSoyUnCallback = () =>{
+
+    console.log('Yo soy un callback');
+}
+
+//MANDAR LLAMAR LA FUNCION INICIAL
+// mostrarUnMensaje(yoSoyUnCallback);
+
+
+// FUNCION INICIAL 2
+ let saludarUsuario = (nombreUsuario, funcion) =>{
+
+    funcion(nombreUsuario);
+ };
+
+ //FUNCION CALLBACK 2
+
+ let callback = (nombreUsuario) =>{
+
+    console.log(`Bienvenido, ${nombreUsuario}`)
+ }
+
+ saludarUsuario('José',callback);
+
+ //Función inicial 3
+
+ let registrarUsuario = (nombre, edad, funcion)=>{
+
+    funcion (nombre, edad);
+ };
+
+ //Función callback 3
+
+ let registroExitoso = (nombre, edad)=>{
+
+    console.log(`Registro Exitoso!!... Su nombre es: ${nombre} y su edad es: ${edad}`);
+ }
+
+ registrarUsuario('José', 29, registroExitoso);
---
+++ 4_async/ejercicios.js
@@ -0,0 +1,59 @@
+
+// 1.- Crear una función de orden superior que reciba como
+//     argumento una variable de cualquier tipo y un callback. 
+// La función de orden superior debe retornar como resultado, 
+// mediante el callback, cual es el tipo de dato de la variable ingresada e 
+// imprimir su contenido.
+
+let tipoDeDato = (variable, func1)=>{
+
+   func1(variable);
+};
+
+let callback = (variable)=>{
+
+    console.log("Datos ingresados: ", variable, " y tipo de dato es:", typeof variable);
+}
+
+// tipoDeDato("Carro",callback);
+
+
+
+// 2.- Crear una función de orden superior que reciba como 
+//     argumentos dos números y un callback. Según el callback
+//     que se pase a la función, se devuelve la suma de los
+//     dos números, la resta de los dos números o la
+//     multiplicación de los dos números.
+
+let op2 = (num1,num2,funcion) => { 
+
+    return funcion (num1, num2);
+};
+
+let sumar = (num1, num2) => {
+
+    console.log("Resultado de la suma:", num1 + num2);
+};
+
+let restar = (num1, num2) => {
+
+    console.log("Resultado de la resta:", num1 - num2);
+};
+
+let multi = (num1, num2) => {
+
+    console.log("Resultado de la multiplicación:", num1 * num2);
+};
+
+op2(3, 4, sumar);
+op2(3, 4, restar);
+op2(3, 4, multi);
+
+
+// 3.- Escribe una función de orden superior que reciba una cadena de
+//     caracteres y debe devolver, mediante un callback, la
+//     cadena de caracteres en mayúsculas o en minúsculas.
+//             ordenSuperior("PejeLagarto", minus);
+//             -> pejelagarto
+//             ordenSuperior("PejeLagarto", mayus);
+//             -> PEJELARTO
