4bd7e38fe4d2736c508a6db72258910236a77bbe
Bruncafc1992 <joseabjerez@hotmail.com>
2021-07-19 23:46:41 -0500

    ejercicio filter

--- 5_funcional/app.js
+++ 5_funcional/app.js
@@ -23,6 +23,10 @@ let nombres = ['Bernardo', 'Juan Antonio', 'Alexander', 'Antonella', 'Junior', '
 
 // nombres.forEach((x,y)=>{console.log(y+1,x, x.length);})
 
+//MAP 
+//iterar arreglos
+//crear arreglos nuevos con el retorno del map
+
 //CONSTRUIR UN ARREGLO BASADO EN UN ARREGLO BASE
 // let a = ['22 - 0', '15 - 1', '10 -2', '12 -3', '8 - 4'];
 let array = [22, 15, 10, 12, 8];
@@ -33,6 +37,30 @@ let nuevoArreglo = array.map((x,y)=>{
 })
 
 
-console.log(nuevoArreglo);
+// console.log(nuevoArreglo);
 
 
+//FILTER
+//nos ayuda a crear un arreglo nuevo que cumpla 
+//con la condición que pongamos en el return
+
+let edadesAlumnos = [20, 21, 30, 10, 15, 8, 30, 1, 20, 30];
+
+let mayores = edadesAlumnos.filter(x=>{
+
+        return x = 30;
+})
+
+
+console.log(edadesAlumnos);
+console.log(mayores);
+
+let animales = ['koala', 'gato', 'araña', 'perro', 'culebra'];
+
+let animalesLength = animales.filter(x =>{
+
+    return x.length <= 5;
+})
+
+
+console.log(animalesLength);
