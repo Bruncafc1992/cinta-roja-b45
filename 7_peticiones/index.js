d0ff24a590d07195b7e93068edfc132b407d1b68
Bruncafc1992 <joseabjerez@hotmail.com>
2021-07-22 00:29:01 -0500

    primeras peticiones a SWAPI

--- 7_peticiones/index.js
+++ 7_peticiones/index.js
@@ -1,13 +1,67 @@
 const request = require('request');
 
-request('https://swapi.dev/api/people/4/', (error, respuesta, body)=>{
 
-    let bodyJSON = JSON.parse(body);
+let obtenerPersonaje = (id) =>{
 
-    console.log(bodyJSON.mass);
-    console.log(bodyJSON.name);
+    request(`https://swapi.dev/api/people/${id}`, (error, respuesta, body)=>{
 
-    console.log(error);
+    if(respuesta.statusCode === 200) {
 
-    console.log(respuesta);
+
+        
+        let bodyJSON = JSON.parse(body);
+        
+        console.log(bodyJSON.name);
+        console.log(bodyJSON.mass);
+    } else{
+
+        console.log( 'Error, status code: ',respuesta.statusCode);
+    }
+
 });
+
+}
+
+obtenerPersonaje(84 );
+
+
+let obtenerPersonajes = ( ) => {
+    request('https://swapi.dev/api/people/', (error, respuesta, body)=>{
+
+
+        if(respuesta.statusCode === 200){
+
+
+            let personajes = JSON.parse(body);
+            
+            let resultados = personajes.results;
+            
+            let nombres = resultados.map( personaje =>  personaje.name);
+            
+            // console.log(nombres);
+            
+            let infoAltura = resultados.map((personaje )=> {
+                
+                return {Nombre: personaje.name, Altura: personaje.height };
+                
+            })
+            
+            console.log(infoAltura);
+            
+            let losMasAltos = infoAltura.filter((x)=>{
+                return x.Altura >= 160;
+                
+            })
+            
+            
+            console.log(losMasAltos);
+        } else {
+
+            console.log(respuesta.statusCode);
+        }
+    
+    
+    })};
+
+
+obtenerPersonajes();
